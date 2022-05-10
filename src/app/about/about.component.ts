b079a2b0931055cb3e307466e1e0dc762f8fde0f
Brandon Ritchie <britchie@avi>
2022-05-10 10:19:27 -0400

    AsyncSubject and ReplaySubject - Learn the Differences

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,6 +11,8 @@ import {
   merge,
   Subject,
   BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
 } from "rxjs";
 import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
@@ -22,20 +24,21 @@ import { createHttpObservable } from "../common/util";
 })
 export class AboutComponent implements OnInit {
   ngOnInit() {
-    const subject = new BehaviorSubject(0);
+    const subject = new ReplaySubject();
 
     const series$ = subject.asObservable();
 
-    series$.subscribe((val) => console.log("early sub: " + val));
+    series$.subscribe((val) => console.log("first sub: " + val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
 
-    subject.complete();
+    // subject.complete();
 
     setTimeout(() => {
-      series$.subscribe((val) => console.log("late sub: " + val));
+      series$.subscribe((val) => console.log("second sub: " + val));
+
       subject.next(4);
     }, 3000);
   }
